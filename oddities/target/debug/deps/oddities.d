c209c0dbb84b83cfbfac432ea69ee2f060eca2c6
nelpats <emalf.pro@gmail.com>
2023-09-26 14:52:17 +0200

    Finished Oddities

---
+++ oddities/target/debug/deps/oddities.d
@@ -0,0 +1,5 @@
+C:\Users\Noah\Documents\Programming\Rust\Rust-Exercices\oddities\target\debug\deps\oddities.exe: src\main.rs
+
+C:\Users\Noah\Documents\Programming\Rust\Rust-Exercices\oddities\target\debug\deps\oddities.d: src\main.rs
+
+src\main.rs:
---
+++ oddities/target/debug/oddities.d
@@ -0,0 +1,1 @@
+C:\Users\Noah\Documents\Programming\Rust\Rust-Exercices\oddities\target\debug\oddities.exe: C:\Users\Noah\Documents\Programming\Rust\Rust-Exercices\oddities\src\main.rs
